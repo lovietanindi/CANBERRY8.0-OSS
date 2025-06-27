47a528e71a45673217fba63741fcb779018c6c1c
lovietanindi <kecilmanyun@gmail.com>
2025-06-28 05:08:16 +0700

    Update

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@
 
 KERNEL_DEFCONFIG=cepheus_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
-FINAL_KERNEL_ZIP=InfiniR_cepheus_v1.39_KSUN.zip
+FINAL_KERNEL_ZIP=runawa@IxoraLT-CANBERRY8.0-OSS.zip
 
 # paths
-TC="/home/runawa/kernel/prebuilts"
+TC=${HOME}/kernel/prebuilts
 
 PATH=${TC}/clang-r416183b1/bin:${TC}/aarch64/bin:${TC}/arm/bin:$PATH
 
@@ -16,6 +16,7 @@ export CC=clang
 export CROSS_COMPILE=aarch64-linux-gnu-
 export ARCH=arm64
 export USE_CCACHE=1
+export DTC_EXT=${HOME}/kernel/dtc/dtc
 
 # Speed up build process
 MAKE="./makeparallel"
@@ -27,13 +28,13 @@ START=$(date +"%s")
 make ARCH=arm64 \
         O=out \
         CC=clang \
-		AR=llvm-ar \
+	AR=llvm-ar \
         LD=ld.lld \
         NM=llvm-nm \
         OBJCOPY=llvm-objcopy \
         OBJDUMP=llvm-objdump \
         STRIP=llvm-strip \
-        -j$(nproc --all)
+        -j$(nproc --all) | tee ${HOME}/CANBERRY8.0-OSS/out/kernel.log
                
 
 echo -e "$yellow**** Verify Image.gz-dtb ****$nocol"
@@ -51,13 +52,13 @@ cp $PWD/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/
 echo -e "$yellow**** Time to zip up! ****$nocol"
 cd $ANYKERNEL3_DIR/
 zip -r9 $FINAL_KERNEL_ZIP * -x README $FINAL_KERNEL_ZIP
-cp $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP /home/runawa/kernel/$FINAL_KERNEL_ZIP
+cp $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP ${HOME}/kernel/$FINAL_KERNEL_ZIP
 
 echo -e "$yellow**** Done, here is your checksum ****$nocol"
 cd ..
 rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
 rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
-rm -rf out/
+#rm -rf out/
 
 END=$(date +"%s")
 DIFF=$((END - START))
